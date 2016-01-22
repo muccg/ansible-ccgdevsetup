6ef4a675d701248b8e8028470bb873dc21240e07
Grahame Bowland <grahame@angrygoats.net>
2016-01-22 12:20:09 +0800

    remove untruth in install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,3 @@ ansible-galaxy install -p ./roles angstwad.docker_ubuntu || true
 ansible-galaxy install -p ./roles joshualund.golang || true
 
 echo "\n\nTo use ansible and annsible-galaxy ensure ${HOME}/.local/bin/ is in \${PATH}"
-echo "I've added it to this session for your convenience"
